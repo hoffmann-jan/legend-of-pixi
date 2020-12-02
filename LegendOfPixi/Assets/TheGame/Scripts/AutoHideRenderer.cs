bddca55fe05982312808a70b30e4a41a518ecfdd
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2020-12-02 21:40:11 +0100

    Add first small dummy level

---
+++ LegendOfPixi/Assets/TheGame/Scripts/AutoHideRenderer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Deactivates the renderer for the gameobject at start.
+/// </summary>
+public class AutoHideRenderer : MonoBehaviour
+{
+    private void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.enabled = false;
+        }
+    }
+}
---
+++ LegendOfPixi/Assets/TheGame/Scripts/CameraMotionController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controller wich keep the hero always in screen.
+/// </summary>
+public class CameraMotionController : MonoBehaviour
+{
+    public Hero Hero;
+
+    private void Update()
+    {
+        Vector3 heroPosition = Hero.transform.position;
+        heroPosition.z = transform.position.z;
+        transform.position = heroPosition;
+    }
+}
