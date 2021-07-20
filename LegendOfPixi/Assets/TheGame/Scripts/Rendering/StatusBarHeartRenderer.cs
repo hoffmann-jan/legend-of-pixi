8be073c5a350bc84d957fee48a93dda777215049
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2021-07-20 13:48:42 +0200

    add status bar hearts

---
+++ LegendOfPixi/Assets/TheGame/Scripts/Rendering/StatusBarHeartRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBarHeartRenderer : MonoBehaviour
+{
+    private Image image;
+
+    public int heartNumber = 1;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (SaveGameDataSingleton.instance.health.Current < heartNumber)
+        {
+            image.color = Color.black;
+        }
+        else
+        {
+            image.color = Color.white;
+        }
+    }
+}
